5090fcf1b95ef70435f4ce8b668cac93437a2e4a
theAD-dev <dev@thead.com.au>
2025-12-09 11:23:51 +0530

    adminOverlyBox

--- src/components/legalvision/muchadmin/index.js
+++ src/components/legalvision/muchadmin/index.js
@@ -2,18 +2,42 @@ import "../../../components/find-one-all/style.css";
 import SliderTab from "../../../components/find-one-all/slider-tabs";
 import SliderTabMobile from "../../../components/find-one-all/slider-mobile";
 import CenterMode from "../../../components/find-one-all/CenterMode";
+import './muchAdmin.css'
 const MuchAdminComponent = () => {
   return (
   <>
   <div className="toMuchAdminSection">
     <p>Too many apps. Too much admin. Too many <br></br>missed  projects or unpaid invoices.</p>
-     <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/tomuchbg.jpg" width="1100px" alt="Mockup" />
+    <div className="adminScrollBackground">
+    <div className="imageAdminBg">
+    <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/backgrounBg-admin.jpg" width="1100px" alt="Mockup" />
+     <div className="overlyAdminText overlyAdminTextFirst">
+       <p>Still jumping between apps just to get one thing done?</p>
+    </div>
+     <div className="overlyAdminText overlyAdminTextSecond">
+       <p>Paperwork’s growing faster than your business?</p>
+    </div>
+     <div className="overlyAdminText overlyAdminTextThird">
+       <p>Still chasing timesheets instead of running the crew?</p>
+    </div>
+     <div className="overlyAdminText overlyAdminTextFourth">
+       <p>Hard to know if you’re making money ‘til the month’s over?</p>
+    </div>
+     <div className="overlyAdminText overlyAdminTextFifth">
+       <p>More jobs, more admin and none of it paid for?</p>
+    </div>
+     
+
+    </div>
+   
+    </div>
+     
     <div className="downClickButton">
         <img alt="DownBlackArrow" src="https://memate-website.s3.ap-southeast-2.amazonaws.com/assets/down-black-arrow.svg"></img>
        </div>
   </div>
   <div className="one-application-wrappers toMuchAdminApplication" >   
-           <span>With MeMate you get<br></br> everything in one place:</span>
+           <span className="text">With MeMate you get<br></br> everything in one place:</span>
       <div className="desktop-slider">
       {/* <SliderTab /> */}
       <CenterMode />
@@ -24,81 +48,7 @@ const MuchAdminComponent = () => {
       </div>
  
     
-       <style jsx>{`
-       
-       .toMuchAdminApplication{
-       padding-bottom: 0;
-       }
-       .toMuchAdminApplication span{
-    color: #29292b;
-    text-align: center;
-    letter-spacing: -2.16px;
-    margin-top: 80px;
-    margin-bottom: 0;
-    font-size: 72px;
-    font-style: normal;
-    font-weight: 420;
-    line-height: 80px;
-    font-family: sequel_sans_semi_bold_disp !important;
-    display: inline-block;
-    width: 100%;
-       }
-  
-      .toMuchAdminSection {
-    text-align: center;
-   
-}
-      .toMuchAdminSection p {
-    color: #29292b;
-    letter-spacing: 2px;
-    margin-bottom: 50px;
-    font-size: 48px;
-    font-style: normal;
-    font-weight: 420;
-    line-height: 55px;
-    text-decoration: none;
-    font-family: sequel_sans !important;
-}
-    .downClickButton{
-     padding-top: 80px;
-     text-align: center;
-    cursor: pointer;
-    width: 100%;
-    display: inline-block;
-    }
 
-
-    @media screen and (max-width: 600px) {
-    .toMuchAdminSection {
-     padding: 0 0px;
-    }
-       .toMuchAdminSection p {
-        margin-bottom: 23px;
-        font-size: 24px;
-        line-height: 30px;
-       
-       }
-        .downClickButton {
-    width: auto;
-    padding-top: 13px;
-}
-       .toMuchAdminSection p br {
-        display: none;
-       }
-       .toMuchAdminSection img {
-        width: 100%;
-       }
-        .toMuchAdminApplication span {
-    letter-spacing: 0;
-    width: 100%;
-    margin-top: 27px;
-    margin-bottom: 0;
-    font-size: 27px;
-    line-height: 31px;
-}
-
-       }
-            `}</style>
   </>
   );
 };
