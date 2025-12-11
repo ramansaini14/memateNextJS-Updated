97c6f07ba4b92b55d667c3e7169ffe3780e8d4e7
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2025-12-11 18:57:23 +0530

    animationHome

--- src/components/logopartner/index.js
+++ src/components/logopartner/index.js
@@ -1,32 +1,40 @@
-import './brandLogoSlider.css'
+import "./brandLogoSlider.css";
+import { motion } from "motion/react";
+
+const LOGOS = [
+  "https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/theadLogo.svg",
+  "https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/legalvisionLogo.svg",
+  "https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/sortedLogo.svg",
+  "https://res.cloudinary.com/dn0jqjad3/image/upload/v1765365094/payComLogo_rfgxh7.svg",
+  "https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/grantHelpLogo.svg",
+];
 
 const BrandLogoSlide = () => {
-    
-    return (
-      <div className="brandLogoSlider">
-        <span>Our partners</span>
-        <div className="brandLogoWrapper">
-      <div className="logoItem">
-          <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/theadLogo.svg" />
-        </div>
-        <div className="logoItem">
-          <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/legalvisionLogo.svg" />
-        </div>
-        <div className="logoItem">
-          <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/sortedLogo.svg" />
-        </div>
-        <div className="logoItem">
-          <img src="https://res.cloudinary.com/dn0jqjad3/image/upload/v1765365094/payComLogo_rfgxh7.svg" />
-        </div>
-        <div className="logoItem">
-          <img src="https://memate-website.s3.ap-southeast-2.amazonaws.com/19-11-2025/grantHelpLogo.svg" />
-        </div>
-   
+  const marqueeLogos = [...LOGOS, ...LOGOS];
+
+  return (
+    <div className="brandLogoSlider">
+      <span>Our partners</span>
+      <div className="brandLogoWrapper">
+        <motion.div
+          className="brandLogoTrack"
+          animate={{ x: ["0%", "-25%"] }}
+          transition={{
+            duration: 90,
+            ease: "linear",
+            repeat: Infinity,
+          }}
+        >
+          {marqueeLogos.map((src, index) => (
+            <div className="logoItem" key={`${src}-${index}`}>
+              <img src={src} alt="Partner logo" draggable={false} />
+            </div>
+          ))}
+        </motion.div>
       </div>
     </div>
-    );
-}
+  );
+};
 
 export default BrandLogoSlide;
-
 
